3f9f25e88b2043412716639d0cb599e3d72803c4
Maksim <Malafeev_maksim@tut.by>
2020-06-03 20:11:12 +0300

    update homework

---
+++ hw3/scripts.js
@@ -0,0 +1,43 @@
+ //----HomeWork 3
+
+ //----Задача №1
+//a)
+
+let i = 1;
+
+while (i <= 50) {
+    console.log(i);
+    i++; 
+}
+
+//b)
+let a = 35;
+
+while (a >= 8) {
+    console.log(a);
+    a--;
+}
+
+// Задача № 2
+
+let b = 89;
+
+while (b >= 11) {
+    document.write(b + '<br/>');
+    b--;
+}
+
+// ---Задача № 3
+let c = 100;
+let sum = 0;
+
+for (let d = 0; d <= c; d++) {
+    console.log(d);
+    sum +=d;
+}
+
+console.log(sum);
+
+//--- Задача № 4
+
+l
