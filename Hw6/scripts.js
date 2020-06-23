3fecc4ef0501d8d7d8c7f23cd9ab36a85dfe0790
Maksim <Malafeev_maksim@tut.by>
2020-06-23 20:37:53 +0300

    update homework

--- Hw6/scripts.js
+++ Hw6/scripts.js
@@ -12,22 +12,22 @@ console.log(result.replace(/-/g, '/'));
 
 let str = 'я учу javascript';
 
-console.log(str.slice(2,5));
-console.log(str.substr(2,3));
-console.log(str.substring(2,5));
+console.log(str.slice(2, 5));
+console.log(str.substr(2, 3));
+console.log(str.substring(2, 5));
 
-console.log(str.slice(6,16));
-console.log(str.substr(6,10));
-console.log(str.substring(16,6));
+console.log(str.slice(6, 16));
+console.log(str.substr(6, 10));
+console.log(str.substring(16, 6));
 
 //----- Задача №4
 
-arr = [4, 2, 5, 19, 13, 0 , 10];
+arr = [4, 2, 5, 19, 13, 0, 10];
 
 let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
-    sum += Math.pow(arr[i], 3) 
+    sum += Math.pow(arr[i], 3)
 }
 
 console.log(Math.sqrt(sum));
@@ -38,13 +38,13 @@ let a = 3,
     b = 5,
     c = Math.abs(a - b);
 
-    console.log(c);
+console.log(c);
 
-    a = 6;
-    b = 1;
-    c = Math.abs(a - b);
+a = 6;
+b = 1;
+c = Math.abs(a - b);
 
-    console.log(c);
+console.log(c);
 
 //------ № 6
 
@@ -57,14 +57,14 @@ let hours = (date.getHours()),
     month = (date.getMonth() + Number(1)),
     year = (date.getFullYear());
 
-    function getZero(number) {
+function getZero(number) {
 
-        if (number >= 0 && number < 10) {
-            return '0' + number;
-        }else {
-            return number;
-        }
+    if (number >= 0 && number < 10) {
+        return '0' + number;
+    } else {
+        return number;
     }
+}
 
 console.log(`${hours}:${minute}:${sec} ${getZero(day)}.${getZero(month)}.${year}`);
 
@@ -80,19 +80,26 @@ let phoneNumber = '+375-29-1243577';
 
 function fastCheck(phone) {
     regexp = /^\+(\d{1,3})-(\d{1,3})-(\d{7,8})$/gi;
-  
+
     if (regexp.test(phone)) {
         return true;
-    }else {
+    } else {
         return false;
     }
 }
- console.log(fastCheck(phoneNumber));
+console.log(fastCheck(phoneNumber));
 
- //---- №9
+//---- №9
 
- let url = 'https://tech.onliner.by/2018/04/26/smart-do-200/?utm_source=main_tile&utm_medium=smartdo200#zag3';
+let url = 'https://tech.onliner.by/2018/04/26/smart-do-200/?utm_source=main_tile&utm_medium=smartdo200#zag3';
 
- 
+function getUrl(url) {
 
+    let domenName = url.match(/http[s]?:\/\/(((\w+)^(\.+)?\w+\-^\w^)^)/g),
+        withoutParametrs = url.match(/(\/(\/?(\w)-^)+\/)/g),
+        parameters = url.match(/(\?)(\w\=^\_^\&^)^/g),
+        hash = url.match(/#(\w^.)^/g);
+    return [domenName, withoutParametrs, parameters, hash];
+}
 
+    console.log(getUrl(url));
